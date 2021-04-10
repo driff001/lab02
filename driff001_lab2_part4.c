b1c42233dd6f04f22335a8efe5262414b8f42375
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-09 19:10:30 -0700

    Update driff001_lab2_part4.c

--- driff001_lab2_part4.c
+++ driff001_lab2_part4.c
@@ -11,7 +11,7 @@ int main(void) {
         unsigned int bruh = 0x00;
       
       
-        
+        	while(1){
 
                  if( (PORTA >= 0x8C) || (PORTB >= 0x8C) || (PORTC >= 0x8C)  ) {
                  PORTD = 0x01;
@@ -30,6 +30,6 @@ int main(void) {
 	         
 		 
 
-
+		 }
                  return 0;
                  }
