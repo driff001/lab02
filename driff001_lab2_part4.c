d9862a41ed81082ff33db13ddd5aca6d96c1575a
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-09 19:38:58 -0700

    Update driff001_lab2_part4.c

--- driff001_lab2_part4.c
+++ driff001_lab2_part4.c
@@ -4,24 +4,24 @@
 #endif
 
 int main(void) {
-        DDRA = 0x00; PINA = 0xFF;
-
-	DDRB = 0xFF; PINB = 0x00;
-        PINC = 0x00; PORTD = 0x00;
+	PINA; PINB; PINC;
+        DDRA = 0x00; PORTA = PINA;
+	DDRB = 0xFF; PORTB = PINB;
+        PORTC = PINC; PORTD = 0x00;
         unsigned int bruh = 0x00;
-	unsigned int result = 0x00;
+	
       
         	while(1){
 
-                 if( (PINA >= 0x8C) || (PINB >= 0x8C) || (PINC >= 0x8C)  ) {
+                 if( (PORTA >= 0x8C) || (PORTB >= 0x8C) || (PORTC >= 0x8C)  ) {
                  PORTD = 0x01;
                  }
 
-	         if ( PINA - PINC > 0x50){
+	         if ( PORTA - PORTC > 0x50){
 		 PORTD = 0x10;
                  }
 
-		 if ( ((PINA >= 0x8C) || (PINB >= 0x8C) || (PINC >= 0x8C)) && (PINA - PINC > 0x50)){  
+		 if ( ((PORTA >= 0x8C) || (PORTB >= 0x8C) || (PORTC >= 0x8C)) && (PORTA - PORTC > 0x50)){  
                  PORTD = 0x11;
 		 }
                
@@ -29,8 +29,8 @@ int main(void) {
 
 		 }
 	         
-		 bruh = PINA + PINB + PINC;
-		 result = (bruh << 8) | PORTD;
-		 PORTD = result;
+		 bruh = PORTA + PORTB + PORTC;
+		
+		 PORTD = bruh;
                  return 0;
                  }
