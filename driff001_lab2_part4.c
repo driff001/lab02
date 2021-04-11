3cf3d193fd43e3421baced06024d9bf2599a69f7
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-10 20:10:43 -0700

    Update driff001_lab2_part4.c

--- driff001_lab2_part4.c
+++ driff001_lab2_part4.c
@@ -19,20 +19,20 @@ int main(void) {
 		 	
 		
 		 if ( ((PINA+PINB+PINC) > 0x8C) && (PINA - PINC > 0x50) && (PINA > PINC) ){  
-                 tempB = 0x11;
+                 tempB = 0b11;
 		 }
 		 if ( ((PINA+PINB+PINC) > 0x8C) && (PINA - PINC > 0x50) && (PINA < PINC) ){  
-                 tempB = 0x10;
+                 tempB = 0b10;
 		 }
                  if( ((PINA+PINB+PINC) > 0x8C)  ) {
-                 tempB = 0x01;
+                 tempB = 0b01;
                  }
 
 	         if ( (PINA-PINC > 0x50)  && (PINA > PINC)){
-		 tempB = 0x10;
+		 tempB = 0b10;
                  }
 		 if ( (PINA-PINC > 0x50)  && (PINA < PINC) ) {
-		 tempB = 0x00;
+		 tempB = 0b00;
                  }
 		
                	 tempA = PINA + PINB + PINC;
