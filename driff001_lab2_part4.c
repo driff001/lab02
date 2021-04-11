f16abc5a9215c56fc0b415220e0e684e1fd1aa1d
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-10 20:06:27 -0700

    Update driff001_lab2_part4.c

--- driff001_lab2_part4.c
+++ driff001_lab2_part4.c
@@ -8,30 +8,30 @@ int main(void) {
         PORTD = 0x00;
         unsigned int tempA = 0x00;
 	unsigned int tempB = 0x00;
-	int minus;
+	
 	
       
         	while(1){
 		 tempB = 0;
 		 tempA = 0;
-		 minus = 0;
 		 
-		 minus = PINA - PINC;	
+		 
+		 	
 		
-		 if ( ((PINA+PINB+PINC) > 0x8C) && (minus > 0x50) && (PINA > PINC) ){  
+		 if ( ((PINA+PINB+PINC) > 0x8C) && (PINA - PINC > 0x50) && (PINA > PINC) ){  
                  tempB = 0x11;
 		 }
-		 else if ( ((PINA+PINB+PINC) > 0x8C) && (minus > 0x50) && (PINA < PINC) ){  
+		 else if ( ((PINA+PINB+PINC) > 0x8C) && (PINA - PINC > 0x50) && (PINA < PINC) ){  
                  tempB = 0x10;
 		 }
                  else if( ((PINA+PINB+PINC) > 0x8C)  ) {
                  tempB = 0x01;
                  }
 
-	         else if ( (minus > 0x50)  && (PINA > PINC)){
+	         else if ( (PINA-PINC > 0x50)  && (PINA > PINC)){
 		 tempB = 0x10;
                  }
-		 else if ( (minus > 0x50)  && (PINA < PINC) ) {
+		 else if ( (PINA-PINC > 0x50)  && (PINA < PINC) ) {
 		 tempB = 0x00;
                  }
 		
